893c716f27d8f913adee96848bb86fccc44b6200
Maria Mendoza <mgmendoza@alumni.usc.edu>
2023-12-03 09:33:14 -0800

    removing older files

--- build/m-explore/map_merge/CMakeFiles/combine_grids.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "/home/mgmendoza/Documents/FinalProjectC206/devel/lib/libcombine_grids.a"
-)
--- build/perception/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mgmendoza/Documents/FinalProjectC206/src/perception
-# Build directory: /home/mgmendoza/Documents/FinalProjectC206/build/perception
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
